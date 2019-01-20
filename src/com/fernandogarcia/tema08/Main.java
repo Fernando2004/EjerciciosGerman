00c83ffa5200766a5107d65ad6794fa988438374
fernando2004 <gmfern2004@gmail.com>
2019-01-20 12:51:01 +0100

    Metemos el main generico

---
+++ src/com/fernandogarcia/tema08/Main.java
@@ -0,0 +1,22 @@
+package com.fernandogarcia.tema08;
+
+import com.fernandogarcia.tema08.arraysPila.PilasString.PilaString;
+import com.fernandogarcia.tema08.ejer6.Bicicleta;
+import com.fernandogarcia.tema08.ejer6.TiendaBicicleta;
+
+public class Main {
+
+    public static void main (String []args) {
+
+        //PilaString pilaString=new PilaString();
+        //pilaString.mainPilaString();
+
+        Bicicleta bicicleta=new Bicicleta();
+        bicicleta.mainBicicleta();
+
+        //Ejer6 ejer6=new Ejer6();
+
+
+
+    }
+}
